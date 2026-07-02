2310248012e67e5a8d5d9ecb4ff87ea5ac681493
Bhavya Srivastava <bhavyasrivastava5april@gmail.com>
2026-07-02 13:20:15 +0530

    Built ResumePilot MVP with Resume Upload and ATS Analysis

--- app.py
+++ app.py
@@ -11,14 +11,15 @@ UPLOAD_FOLDER = os.path.join(BASE_DIR, 'uploads')
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def home():
 
     if request.method == 'POST':
 
         file = request.files['resume']
+        job_description = request.form['job_description']
 
         filepath = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-
         file.save(filepath)
 
         reader = PdfReader(filepath)
@@ -30,7 +31,28 @@ def home():
             if text:
                 resume_text += text
 
-        return resume_text
+        # Convert both to lowercase
+        resume_text = resume_text.lower()
+        job_description = job_description.lower()
+
+        # Split into words
+        resume_words = set(resume_text.split())
+        job_words = set(job_description.split())
+
+        # Find matching words
+        matched_words = resume_words.intersection(job_words)
+
+        # Calculate score
+        if len(job_words) != 0:
+            score = round((len(matched_words) / len(job_words)) * 100, 2)
+        else:
+            score = 0
+
+        return render_template(
+            "result.html",
+            score=score,
+            matched=sorted(matched_words)
+        )
 
     return render_template('index.html')
 

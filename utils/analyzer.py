3503fad64df82038c39f983b2846041cf7a66b4d
Bhavya Srivastava <bhavyasrivastava5april@gmail.com>
2026-07-16 22:32:13 +0530

    Improved homepage and result page layout

--- utils/analyzer.py
+++ utils/analyzer.py
@@ -7,14 +7,15 @@ def calculate_ats_score(resume_text, job_description):
     resume_skills = extract_skills(resume_text)
     job_skills = extract_skills(job_description)
 
-    print("Resume Skills:", resume_skills)
-    print("Job Skills:", job_skills)
-
     # Find common skills
     matched_skills = list(set(resume_skills) & set(job_skills))
 
     # Find missing skills
     missing_skills = list(set(job_skills) - set(resume_skills))
+    print("Resume Skills:", resume_skills)
+    print("Job Skills:", job_skills)
+    print("Matched Skills:", matched_skills)
+    print("Missing Skills:", missing_skills)
 
     # Calculate ATS Score
     if len(job_skills) > 0:
